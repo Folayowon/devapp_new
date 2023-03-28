fd90e51f4166f1a0f62213db769187aea1991f51
MaXcript <folayowonwisdom@gmail.com>
2023-03-28 20:36:47 +0100

    hello baby

--- context/constants.js
+++ context/constants.js
@@ -1,7 +1,7 @@
 //organizerAddress = '0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266'
 
 // Set the address of the deployed Voting contract
-export const VotingAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+export const VotingAddress = "0x1339c74faa254052898668F0ac444Bd918D33f59";
 
 // Set the ABI (Application Binary Interface) of the Voting contract
 // The ABI is used by ethers.js to interact with the deployed contract
